3d721a5c633ec2843795e4cdbc4e851654dfc38c
Emir Risyad <emirrsyd@gmail.com>
2023-04-27 13:56:38 +0200

    part1 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+  function Statistics({ good, neutral, bad }) {
+    const total = good + neutral + bad;
+    const average = (good - bad) / total;
+    const percentage = (good / total) * 100;
+
+
+    if (total === 0) {
+      return <p>No feedback given</p>;
+    }
+    return (
+      <>
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="total" value={total} />
+        <StatisticsLine text="average" value={average} />
+        <StatisticsLine text="percentage of positive %" value={percentage} />
+      </>
+    );
+  }
+
+  function StatisticsLine(props) {
+    return (
+      <p>
+        {props.text} {props.value}
       </p>
-    </>
-  )
-}
+    );
+  }
 
-export default App
+  function Button({ handleClick, text }) {
+    return <button onClick={handleClick}>{text}</button>;
+  }
+
+  function handleGoodClick() {
+    setGood(good + 1);
+    console.log(good);
+  }
+
+  function handleNeutralClick() {
+    setNeutral(neutral + 1);
+    console.log(neutral);
+  }
+
+  function handleBadClick() {
+    setBad(bad + 1);
+    console.log(bad);
+  }
+  return (
+    <div>
+      <h2>Give feedback</h2>
+      <Button handleClick={handleGoodClick} text="Good" />
+      <Button handleClick={handleNeutralClick} text="Neutral" />
+      <Button handleClick={handleBadClick} text="Bad" />
+
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+}
